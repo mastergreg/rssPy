3934b3138b254cd01831a207e50b80ef0f4e20e9
Gregory Liras <gregliras@gmail.com>
2012-03-26 11:14:05 +0300

    init

---
+++ rssPy.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8
+#
+#* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+# File Name : rssPy.py
+# Creation Date : 26-03-2012
+# Last Modified : Mon 26 Mar 2012 10:59:10 AM EEST
+# Created By : Greg Liras <gregliras@gmail.com>
+#_._._._._._._._._._._._._._._._._._._._._.*/
+from sys import argv
+from urllib2 import Request, urlopen
+from re import search,match,compile,findall,sub
+
+def main():
+    if not len(argv) >= 2:
+        print "Usage: %s <url> <titles>"%argv[0]
+    req = Request(argv[1])
+    response = urlopen(req)
+    the_page = response.read()
+    regexp = compile(r'<title.*?\/title>')
+    titles = findall(regexp,the_page)
+    regexp = compile(r'</?title[^>]*>')
+    titles = map(lambda s:sub(regexp, '',s),titles)
+    for i in range(int(argv[2])):
+        print titles[i]
+    
+
+
+
+if __name__=="__main__":
+    main()
+
