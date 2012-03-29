264ff2ebbeb94ea846c0e60b9ab6502b689c3587
Gregory Liras <gregliras@gmail.com>
2012-03-29 09:01:37 +0300

    it's not a bot...

--- rssPy.py
+++ rssPy.py
@@ -4,7 +4,7 @@
 #* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 # File Name : rssPy.py
 # Creation Date : 26-03-2012
-# Last Modified : Wed 28 Mar 2012 03:12:02 AM EEST
+# Last Modified : Thu 29 Mar 2012 09:01:17 AM EEST
 # Created By : Greg Liras <gregliras@gmail.com>
 #_._._._._._._._._._._._._._._._._._._._._.*/
 from sys import argv
@@ -14,7 +14,7 @@ from re import compile,findall,sub
 def main():
     if not len(argv) >= 2:
         print "Usage: %s <url> <titles>"%argv[0]
-    req = Request(argv[1])
+    req = Request(argv[1],headers={'User-agent':'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'})
     response = urlopen(req)
     the_page = response.read()
     regexp = compile(r'<title.*?\/title>')
